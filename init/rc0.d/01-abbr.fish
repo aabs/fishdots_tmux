c0adfde188c41190d9d37125ddc6b2adf8fa6ea8
Andrew Matthews <matthews.andrew@gmail.com>
2018-03-02 04:31:11 +1100

    initial import

---
+++ init/rc0.d/01-abbr.fish
@@ -0,0 +1,6 @@
+#tl: list sessions
+abbr --add tl   'tmux ls'
+#tn <name>: create a session named <name>
+abbr --add tn   'tmux -2 new -s'
+#ta <name>: attach to a session named <name>
+abbr --add ta   'tmux -2 attach -t'
