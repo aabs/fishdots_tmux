b38f1b94d062e918921a15df26c77e32586bb758
Andrew Matthews <matthews.andrew@gmail.com>
2020-04-11 10:00:35 +1000

    installer

--- install.fish
+++ install.fish
@@ -1,3 +1,11 @@
 #!/usr/bin/env fish
 
-ln -sf $FISHDOTS_PLUGINS_HOME/fishdots_tmux/tmux.conf.symlink ~/.tmux.conf
+if not test -d $HOME/.tmux/plugins/tpm
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+end
+
+if not test -e $HOME/.tmux.conf
+  ln -sf $FISHDOTS_PLUGINS_HOME/fishdots_tmux/tmux.conf.symlink $HOME/.tmux.conf
+end
+
+
